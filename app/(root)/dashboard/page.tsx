d13407f006f1333152f8ab218bca4cd8d200cf09
Boy-Zee <felixhosea61@gmail.com>
2025-07-29 09:17:41 +0100

    finished dashboard for now, also added a few components to resume manager

--- app/(root)/dashboard/page.tsx
+++ app/(root)/dashboard/page.tsx
@@ -1,16 +1,29 @@
+'use client'
+
 import UserData from '@/app/(root)/dashboard/components/userData'
-import React from 'react'
+import React, { useEffect } from 'react'
 import Notifications from './components/notifications'
 import QuickActions from './components/quickActions'
 import RecentActivity from './components/recentActivity'
 import BiggerNotification from './components/biggerNotification'
 import Image from 'next/image'
+import { useCurrentNav } from '@/store/store'
 
 const Page = () => {
+
+  const notifications = [{
+    notification:'this is a dummy notification',
+    severity:"casual"
+  }]
+
+  useEffect(()=>{
+    useCurrentNav.getState().setCurrentNav("Dashboard")
+  }, [])
+
   return (
     <div className='w-full h-full relative flex flex-col gap-10 pb-20'>
       <UserData/>
-      <Notifications/>
+      <Notifications notifications={notifications}/>
       <QuickActions/>
       <RecentActivity/>
       <BiggerNotification/>
